4b53d0d2f2ebde6550f76ed4bad952d5bb7fb266
CaioRezende <caiorezende2008@hotmail.com>
2017-02-22 10:09:10 -0300

    Exercícios LP2 - 22/02/17

---
+++ Ponto.java
@@ -0,0 +1,45 @@
+public class Ponto{
+	private double x,y;
+		public void setX(double x){
+			this.x = x;
+}
+		public double getX(){
+			return x;
+}
+		public void setY(double y){
+			this.y = y;
+}
+		public double getY(){
+			return y;
+}
+
+
+
+	public static void main(String[] args){
+		System.out.println("Corinthians é timinho");
+		Ponto p1 = new Ponto();
+			p1.setX(10);
+			p1.setY(20);
+		Ponto p2 = new Ponto();
+			p2.setX(15);
+			p2.setY(30);
+		Ponto p3 = new Ponto();
+			p3.setX(50);
+			p3.setY(70);
+		p3 = p1;
+		Ponto p4 = p3;
+		System.out.println(p4.getX()+p4.getY());
+		System.out.println(p4.getY());
+		System.out.println(p1);
+}
+
+}
+
+
+
+
+
+
+
+
+
