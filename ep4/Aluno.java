c9bab4c74cdc6f58bb420970b5e897bd19592255
CaioRezende <caiorezende2008@hotmail.com>
2017-04-02 19:11:12 -0300

    Update Aluno.java

--- ep4/Aluno.java
+++ ep4/Aluno.java
@@ -1,14 +1,18 @@
 public class Aluno{
+	
 	private String nome;
 	private int idade;
+	
 	public Aluno(String nome, int idade){
 		this.nome = nome;
 		this.idade = idade;
 	}
-	public String getNome(){
-		return this.nome;
-	}
 	public int getIdade(){
 		return this.idade;
 	}
+	
+	public String getNome(){
+		return this.nome;
+	}
+	
 }
