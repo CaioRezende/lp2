3e415eeaf5f7ae9c023b694e9c7c763271625b79
CaioRezende <caiorezende2008@hotmail.com>
2017-04-02 21:03:16 -0300

    Lista EP2

--- ep2/Retangulo.java
+++ ep2/Retangulo.java
@@ -1,14 +1,31 @@
-Class Retangulo(){
-	Int perimetro, area;
-	setPerimetro(int lado){
-		this.perimetro = ((2*get.Base())+(2*get.Altura()));
-}
-	getPerimetro(){
-		return this.perimetro;
-}
-	setArea()
-		this.area = get.Base()*get.Altura();
-}
-	getArea(){
-		return this.area;
+public class Retangulo extends FiguraGeometrica(){
+	
+	private double l1, l2;
+
+	public void setL1(double l1){
+		this.l1=l1;
+	}
+
+	public void setL2(double l2){
+		this.l2=l2;
+		}
+	
+	public double getL1(){
+		return l1;
+		}	
+		
+	public double getL2(){
+		return l2;
+		}	
+	
+	
+	public double Pretangulo(){
+		return ((getL1()*2)+(getL2()*2));
+		}
+	
+	public double Aretangulo(){
+		return getL1()*getL2();
+		}
+		
+			
 }
